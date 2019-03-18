d92a73eaf793e04c4d85dcab2979e968ab04ec6d
IFctor <viktor.madi@gmail.com>
2019-03-18 09:16:35 +0100

    S -> D

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+package designPatterns.cart;
+
 import designPatterns.cart.model.Cart;
 import designPatterns.cart.model.Item;
 
@@ -6,14 +8,14 @@ public class Main {
     public static void main(String[] args)
     {
         Cart myCart = new Cart(1);
-        myCart.setSale(10);
+        myCart.setDiscount(10);
         Item laptop = new Item(1, "laptop", 12125101, 135000, 10);
         myCart.addItem(laptop);
         Cart myNestedCart = new Cart (2, "printer+laptop");
-        myNestedCart.setSale(0);
-        Item myNestedLaptop = new Item (1,"laptop",682786711,100000,0);
+        myNestedCart.setDiscount(25);
+        Item myNestedLaptop = new Item (1,"laptop",682786711,135000,0);
         myNestedCart.addItem(myNestedLaptop);
-        Item myNestedPrinter = new Item (2,"printer",64565446,150000,0);
+        Item myNestedPrinter = new Item (2,"printer",64565446,50000,0);
         myNestedCart.addItem(myNestedPrinter);
         myCart.addItem(myNestedCart);
         System.out.println(myCart.toString());
